39d38a5c9b87a8da65fd812a72600799c30a2f05
Andrew Arrow <oneone@gmail.com>
2023-07-12 14:58:50 -0700

    run

--- backups/run.go
+++ backups/run.go
@@ -13,12 +13,21 @@ import (
 )
 
 func Run(db string) {
+	for {
+		fmt.Println("Running")
+		time.Sleep(time.Hour * 24)
+		RunOnce(db)
+	}
+}
+
+func RunOnce(db string) {
 	now := time.Now().Unix()
 	filename := fmt.Sprintf("%s_%d.sql", db, now)
 	b, err := exec.Command("bash", "-c", fmt.Sprintf("pg_dump postgres://fred:fred@localhost:5432/%s > %s", db, filename)).CombinedOutput()
 	fmt.Println(string(b), err)
 	b, err = exec.Command("gzip", filename).CombinedOutput()
 	fmt.Println(string(b), err)
+	filename = filename + ".gz"
 	asBytes, _ := ioutil.ReadFile(filename)
 	storeInBucket(asBytes, filename)
 }
@@ -29,7 +38,7 @@ func storeInBucket(data []byte, filename string) {
 
 	gcsClient, err := storage.NewClient(context.Background(),
 		option.WithCredentialsFile(keyPath))
-	fmt.Println(err, bucket, keyPath)
+	fmt.Println(err, bucket, keyPath, len(data), filename)
 
 	w := gcsClient.Bucket(bucket).Object(filename).NewWriter(context.Background())
 	w.ContentType = "application/octet-stream"
