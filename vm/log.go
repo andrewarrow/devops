a2c3a8bb9da6fcf6e80b9f7c7e5a6530e6ed3de9
Andrew Arrow <oneone@gmail.com>
2024-02-01 07:04:09 -0800

    Use the primary ADP bus, then you can program the cross-platform microchip!

--- vm/log.go
+++ vm/log.go
@@ -10,9 +10,10 @@ func GetLog(service string) {
 	pem := os.Getenv("SSH_PEM")
 	who := os.Getenv("SSH_USER")
 	ip := os.Getenv("VM_IP")
-	item := `sudo journalctl -u %s.service --since "1 hour ago" > /tmp/t; cat /tmp/t`
+	item := `sudo journalctl -u %s.service --since=today > /tmp/t
+	sudo cat /tmp/t`
 	send := fmt.Sprintf(item, service)
 	b, err := exec.Command("ssh", "-i", pem, who+"@"+ip,
-		"bash -s", "<<<", send).CombinedOutput()
+		"<<<", send).CombinedOutput()
 	fmt.Println(string(b), err == nil)
 }
